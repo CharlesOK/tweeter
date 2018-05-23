ad2d140a0c2a4c5cb1a810c633179dbec9d1af4a
RadiophonicOddity09 <charlesokwechime@gmail.com>
2018-05-23 22:11:41 +0000

    created a function that appeends tweets to the body

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,63 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+function createTweetElement(tweetData) {
+  const $tweet = $('<article>')
+  .addClass('tweet');
+
+  const $header = $('<header>')
+  .addClass('tweet-header')
+  .appendTo($tweet);
+
+  const $div = $('<div>')
+  .addClass('clearfix')
+  .appendTo($header);
+
+  const $img = $('<img>')
+  .addClass('photo')
+  .attr('src', tweetData.user.avatars.small )
+  .appendTo($div);
+
+  const $span = $('<span>')
+  .addClass('name')
+  .text(tweetData.user.name)
+  .appendTo($img);
+
+  const $span = $('<span>')
+  .addClass('user')
+  .text(tweetData.user.handle)
+  .appendTo($img);
+
+  const $p = $('<p>')
+  .addClass('text')
+  .text(tweetData.content.text)
+  .appendTo($tweet);
+
+  const $footer = $('<footer>')
+  .addClass('tweet-footer')
+  .appendTo($tweet);
+
+  const $span = $('<span>')
+  .addClass('icons')
+  .appendTo($tweet);
+
+  const $i = $('<i>')
+  .addClass('fas fa-flag')
+  .appendTo($span);
+
+  const $i = $('<i>')
+  .addClass('fas fa-retweet')
+  .appendTo($span);
+
+  const $i = $('<i>')
+  .addClass('fas fa-heart')
+  .appendTo($span);
+
+  return $tweet;
+}
+
+function renderTweets(tweets) {
+  tweets.forEach(function (singleTweet) {
+    $('#tweet-list').prepend(createTweetElement(singleTweet));
+  });
+}
