34ea99f966d7836b9e212ea7c376d7b1cea6e3a1
RadiophonicOddity09 <charlesokwechime@gmail.com>
2018-05-23 20:14:44 +0000

    used let instead of var

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,8 +2,8 @@
 // Character counting function
 $(document).ready(function() {
   $('.new-tweet textarea').on('input', function() {
-    var maxCharacters = 140;
-    var character = $(this).val().length;
+    let maxCharacters = 140;
+    let character = $(this).val().length;
     $(this).parent().find('.counter').text(maxCharacters - character);
     if(maxCharacters - character < 0) {
       $('.counter').addClass('redText');
