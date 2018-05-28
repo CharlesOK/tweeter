9f60db851b6ff51d307ffc6dc24045c2e6698a6d
RadiophonicOddity09 <charlesokwechime@gmail.com>
2018-05-28 15:51:57 +0000

    fixed issue with database routing

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.static("public"));
 // The in-memory database of tweets. It's a basic object with an array in it.
 const Mongo = require("mongodb");
 const MongoClient = require("mongodb").MongoClient;
-const MONGODB_URI = "mongodb://localhost:27017//tweeter";
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
 // The `data-helpers` module provides an interface to the database of tweets.
 // This simple interface layer has a big benefit: we could switch out the
@@ -26,8 +26,9 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
   //if(err) throw err;
 
   console.log(`Connected to mongdb: ${MONGODB_URI}`);
-
-  const DataHelpers = require("./lib/data-helpers.js")(db);
+  console.log(db);
+  let createDataHelpers = require("./lib/data-helpers.js");
+  const DataHelpers = createDataHelpers(db);
 
 // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
 // so it can define routes that use it to interact with the data layer.
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -5,12 +5,13 @@ const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
+  console.log("data helpers initialized with ", db);
   return {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
       simulateDelay(() => {
-        db.collection("tweets").insertOne(newTweet, callback);
+        db.collection('tweets').insertOne(newTweet, callback);
         callback(null, true);
       });
     },
@@ -19,7 +20,8 @@ module.exports = function makeDataHelpers(db) {
     getTweets: function(callback) {
       simulateDelay(() => {
         const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        var collection = db.collection("tweets");
+        console.log(db);
+        var collection = db.collection('tweets');
         collection.find({}).toArray(function(err, results) {
           callback(null, results.sort(sortNewestFirst));
         });
